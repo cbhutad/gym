3a74c87479b00692482d51ac9fad2e36a559f7bf
cbhutad <learningcbhutad@gmail.com>
2025-01-07 15:58:22 +0530

    test code for subset token generation

--- compilers/teeny_tiny/src/test/java/com/github/cbhutad/gym/compilers/LexerTest.java
+++ compilers/teeny_tiny/src/test/java/com/github/cbhutad/gym/compilers/LexerTest.java
@@ -1,8 +1,12 @@
 package com.github.cbhutad.gym.compilers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class LexerTest {
 
@@ -20,4 +24,21 @@ public class LexerTest {
 
 		assertTrue(source.equals(sb.toString()));
     }
+
+	@Test
+	public void testOperators() {
+		String source = "+/*-";
+		List<String> result = new ArrayList<>();
+		List<String> expected = Arrays.asList("PLUS", "SLASH", "ASTERISK", "MINUS", "NEWLINE");
+
+		Lexer lex = new Lexer(source);
+		Token token = lex.getToken();
+
+		while (token.getKind() != TokenType.EOF) {
+			result.add(token.toString());
+			token = lex.getToken();
+		}
+
+		assertEquals(expected, result);
+	}
 }
