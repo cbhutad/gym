ae7c87e9cf8f697eb0ab72d4cb91cc91bf064a1c
cbhutad <learningcbhutad@gmail.com>
2025-01-07 15:57:30 +0530

    Token classifications

---
+++ compilers/teeny_tiny/src/main/java/com/github/cbhutad/gym/compilers/TokenType.java
@@ -0,0 +1,46 @@
+package com.github.cbhutad.gym.compilers;
+
+public enum TokenType {
+
+    EOF(-1),
+    NEWLINE(0),
+    NUMBER(1),
+    IDENT(2),
+    STRING(3),
+    
+    // Keywords
+    LABEL(101),
+    GOTO(102),
+    PRINT(103),
+    INPUT(104),
+    LET(105),
+    IF(106),
+    THEN(107),
+    ENDIF(108),
+    WHILE(109),
+    REPEAT(110),
+    ENDWHILE(111),
+    
+    // Operators
+    EQ(201),
+    PLUS(202),
+    MINUS(203),
+    ASTERISK(204),
+    SLASH(205),
+    EQEQ(206),
+    NOTEQ(207),
+    LT(208),
+    LTEQ(209),
+    GT(210),
+    GTEQ(211);
+
+    private final int tokenValue;
+
+    private TokenType(int tokenValue) {
+        this.tokenValue = tokenValue;
+    }
+
+    public int getTokenValue() {
+        return tokenValue;
+    }
+}
