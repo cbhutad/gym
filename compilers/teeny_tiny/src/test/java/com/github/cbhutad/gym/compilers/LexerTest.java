bba58a2a20ca8c41824f0d35d1feba2c795e909d
cbhutad <learningcbhutad@gmail.com>
2025-01-09 11:48:28 +0530

    test code for comment added

--- compilers/teeny_tiny/src/test/java/com/github/cbhutad/gym/compilers/LexerTest.java
+++ compilers/teeny_tiny/src/test/java/com/github/cbhutad/gym/compilers/LexerTest.java
@@ -58,5 +58,22 @@ public class LexerTest {
 
 		assertEquals(expected, result);
 	}
+
+	@Test
+	public void testComments() {
+		String source = "+/ # This is a comment\n *-";
+		List<String> result = new ArrayList<>();
+		List<String> expected = Arrays.asList("PLUS", "SLASH", "NEWLINE", "ASTERISK", "MINUS", "NEWLINE");
+
+		Lexer lex = new Lexer(source);
+		Token token = lex.getToken();
+
+		while (token.getKind() != TokenType.EOF) {
+			result.add(token.toString());
+			token = lex.getToken();
+		}
+
+		assertEquals(expected, result);
+	}
 	
 }
