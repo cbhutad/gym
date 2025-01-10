06cab75258352202e03a45e4ea8cfb7f950d1e2b
cbhutad <learningcbhutad@gmail.com>
2025-01-10 12:50:24 +0530

    reading tiny extension files support added

--- compilers/teeny_tiny/src/main/java/com/github/cbhutad/gym/compilers/App.java
+++ compilers/teeny_tiny/src/main/java/com/github/cbhutad/gym/compilers/App.java
@@ -1,15 +1,41 @@
 package com.github.cbhutad.gym.compilers;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class App {
 
     public static void main(String[] args) {
 
-        Lexer lex = new Lexer(args[0]);
-        Token token = lex.getToken();
+        File file = new File(args[0]);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            StringBuilder sb = new StringBuilder();
 
-        while (token.getKind() != TokenType.EOF) {
-            System.out.println(token.toString());
-            token = lex.getToken();
+            while (br.ready()) {
+                sb.append(br.readLine());
+            }
+
+            Lexer lex = new Lexer(sb.toString());
+            Token token = lex.getToken();
+    
+            while (token.getKind() != TokenType.EOF) {
+                System.out.println(token.toString());
+                token = lex.getToken();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (file.exists()) {
+               try {
+                   br.close();   
+               } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            } 
         }
     }
 
