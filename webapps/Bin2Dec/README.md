7d1315855d6664721d435922bcc2288917a41cb6
cbhutad <learningcbhutad@gmail.com>
2025-01-23 12:41:13 +0530

    initial commit

---
+++ webapps/Bin2Dec/README.md
@@ -0,0 +1,3 @@
+# Binary to Decimal Number Converter
+
+This is the solution to the [app idea](https://github.com/backupdev-main/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md). 
---
+++ webapps/Bin2Dec/script.js
@@ -0,0 +1,29 @@
+const resultSpan = document.getElementById("result");
+const errorMessageSpan = document.getElementById("error-message");
+
+function generateDecimal() {
+
+	// fetch the input string provided by user.
+	const binaryInput = document.getElementById("binarystring").value.trim();
+
+	// check for binary constraint of the string.
+	for (let i = 0;i < binaryInput.length; i++) {
+		if (binaryInput[i] != '1' && binaryInput[i] != '0') {
+			errorMessageSpan.innerText = "binary string does not allow integers apart from 0 and 1";
+			return;
+		}
+	}
+	
+	// calculate the equivalent decimal value.
+	let power = 0;
+	let decimalValue = 0;
+	
+	for (let i = binaryInput.length - 1;i >= 0; i--) {
+		if (binaryInput[i] == '1') {
+			decimalValue = decimalValue + (2 ** power);
+		}
+		power++;
+	}
+
+	resultSpan.innerText = "Decimal Equivalent : " + decimalValue;
+}
