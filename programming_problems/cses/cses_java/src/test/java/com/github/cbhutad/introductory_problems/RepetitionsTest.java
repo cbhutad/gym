60b5ccbd332b8d112efd5e4e83e7ab863c5aceb1
cbhutad <learningcbhutad@gmail.com>
2025-04-15 22:03:40 +0530

    additional tests

--- programming_problems/cses/cses_java/src/test/java/com/github/cbhutad/introductory_problems/RepetitionsTest.java
+++ programming_problems/cses/cses_java/src/test/java/com/github/cbhutad/introductory_problems/RepetitionsTest.java
@@ -10,4 +10,34 @@ public class RepetitionsTest {
         long count = Repetitions.solution("ATTCGGGA");
         assertEquals(3, count);
     }
+
+    @Test
+    public void testcase2() {
+        long count = Repetitions.solution("ACACACACAC");
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void testcase3() {
+        long count = Repetitions.solution("ACCGGGTTTT");
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void testcase4() {
+        long count = Repetitions.solution("AAAACCCGGT");
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void testcase5() {
+        long count = Repetitions.solution("CTCAGGTCCG");
+        assertEquals(2, count);
+    }
+
+    @Test
+    public void testcase6() {
+        long count = Repetitions.solution("A");
+        assertEquals(1, count);
+    }
 }
