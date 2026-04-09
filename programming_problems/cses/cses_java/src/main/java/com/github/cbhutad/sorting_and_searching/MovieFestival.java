cdc1764a360b8fcad3d952ba2cd95e5fcba90c8b
cbhutad <learningcbhutad@gmail.com>
2026-04-09 22:15:36 +0530

    added package statement

--- programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/sorting_and_searching/MovieFestival.java
+++ programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/sorting_and_searching/MovieFestival.java
@@ -1,12 +1,11 @@
+package com.github.cbhutad.sorting_and_searching;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.TreeMap;
 
 public class MovieFestival {
     
