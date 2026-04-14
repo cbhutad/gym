3ccf996ce7bef86c5456fbdfaa1c75000dec10f0
cbhutad <learningcbhutad@gmail.com>
2026-04-14 14:41:44 +0530

    another approach to Gray Code problem

--- programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/introductory_problems/GrayCode.java
+++ programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/introductory_problems/GrayCode.java
@@ -4,27 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class GrayCode {
     public static List<String> solution(int input) {
-        List<String> results = new ArrayList<>();
-        Long value = (long)Math.pow(2, input);
-		
-		for (long i = 0;i < value;i++) {
-			String result = Long.toBinaryString(i);
-			if (result.length() < input) {
-				result = "0".repeat(input - result.length()) + result;
-			}
-			results.add(result);
-			result = "";
-		}
-        return results;
+
+        if (input == 1) {
+            return Arrays.asList("0", "1");
+        }
+
+        List<String> code = new ArrayList<>();
+        List<String> prev = solution(input - 1);
+
+        for (String s : prev) {
+            code.add("0" + s);
+        }
+
+        Collections.reverse(prev);
+
+        for (String s : prev) {
+            code.add("1" + s);
+        }
+
+        return code;
     }
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        solution(n);
+        List<String> entries = solution(n);
+        for (String entry : entries) {
+            System.out.println(entry);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
     }
 }
