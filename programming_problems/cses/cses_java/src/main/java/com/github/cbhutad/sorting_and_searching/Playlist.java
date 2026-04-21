f6866f13ffaecc16c229a2f2c5a41c734afdc6e3
cbhutad <learningcbhutad@gmail.com>
2026-04-21 23:05:44 +0530

    completed Playlist problem

--- programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/sorting_and_searching/Playlist.java
+++ programming_problems/cses/cses_java/src/main/java/com/github/cbhutad/sorting_and_searching/Playlist.java
@@ -1,9 +1,9 @@
+package com.github.cbhutad.sorting_and_searching;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 
 public class Playlist {
     public static void main(String[] args) throws IOException {
